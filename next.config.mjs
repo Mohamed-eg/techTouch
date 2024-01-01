0db5822dcb29d44d15f2fe2eda157f51f0f49df2
Mohamed-eg <mhmdalshayb39@gmail.com>
2024-01-01 15:48:16 +0200

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  output: "export",
+  reactStrictMode: true,
+  distDir: "build",
 };
 
 export default nextConfig;
