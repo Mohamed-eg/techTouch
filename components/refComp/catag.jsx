17e99d13745f4901056dd3f7ec71cbf25fd8b0b7
Mohamed-eg <mhmdalshayb39@gmail.com>
2024-02-13 14:11:04 +0200

    p

--- components/refComp/catag.jsx
+++ components/refComp/catag.jsx
@@ -139,157 +139,9 @@ export default function Categorypage() {
     <div className="bg-white">
       <MainHeader />
       <div>
-        {/* Mobile filter dialog */}
-        {/* <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-40 lg:hidden"
-            onClose={setMobileFiltersOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0">
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
+        {/* Mobile filter dialog */} 
 
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full">
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
-                  <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">
-                      Filters
-                    </h2>
-                    <button
-                      type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                      onClick={() => setMobileFiltersOpen(true)}>
-                      <span className="sr-only">Close menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div> */}
-
-                  {/* Filters */}
-                  {/* <form className="mt-4 border-t border-gray-200">
-                    <h3 className="sr-only">Categories</h3> */}
-                    {/* <ul
-                      role="list"
-                      className="px-2 py-3 font-medium text-gray-900">
-                      {subCategories.map((category) => (
-                        <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
-                            {category.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul> */}
-
-
-                      {/* <Disclosure
-                        as="div"
-                        key={"mobilecolor"}
-                        className="border-t border-gray-200 px-4 py-6">
-                        {({ open }) => (
-                          <>
-                            <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                <span className="font-medium text-gray-900">
-                                  color
-                                </span>
-                                <span className="ml-6 flex items-center">
-                                  {open ? (
-                                    <MinusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <PlusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                </span>
-                              </Disclosure.Button>
-                            </h3>
-                            <Disclosure.Panel className="pt-6">
-                              <div className="space-y-6">
-                              <ColorPicker color={color} onChange={setColor} />;
-                              </div>
-                            </Disclosure.Panel>
-                          </>
-                        )}
-                      </Disclosure>
-                      <Disclosure
-                        as="div"
-                        key={"mobileid"}
-                        className="border-t border-gray-200 px-4 py-6">
-                        {({ open }) => (
-                          <>
-                            <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                <span className="font-medium text-gray-900">
-                                  category
-                                </span>
-                                <span className="ml-6 flex items-center">
-                                  {open ? (
-                                    <MinusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <PlusIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                </span>
-                              </Disclosure.Button>
-                            </h3>
-                            <Disclosure.Panel className="pt-6">
-                              <div className="space-y-6">
-                                {categories.map((option, optionIdx) => (
-                                  <div
-                                    key={option.title}
-                                    className="flex items-center">
-                                    <input
-                                      id={`filter-mobile-${option.id}-${optionIdx}`}
-                                      name={`${option.id}[]`}
-                                      defaultValue={option.title}
-                                      type="checkbox"
-                                      defaultChecked={true}
-                                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                    />
-                                    <label
-                                      htmlFor={`filter-mobile-${option.id}-${optionIdx}`}
-                                      className="ml-3 min-w-0 flex-1 text-gray-500">
-                                      {option.title}
-                                    </label>
-                                  </div>
-                                ))}
-                              </div>
-                            </Disclosure.Panel>
-                          </>
-                        )}
-                      </Disclosure>
-  
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root> */}
-
-        <main className="mx-auto mt-[200px] max-w-7xl  rounded-xl px-4 sm:px-6 lg:px-8">
+        <main className="mx-auto mt-[200px] max-w-7xl  px-4 sm:px-6 lg:px-8">
           <div className="w-full !flex justify-center  rounded-xl items-center overflow-hidden">
             <Swiper
               className="!flex justify-center  !p-0 rounded-xl !w-[100vw] items-center"
@@ -299,35 +151,35 @@ export default function Categorypage() {
               <SwiperSlide className=" rounded-xl">
                 <Image
                   alt="img"
-                  className="w-full h-auto object-contain rounded-lg"
+                  className="w-full h-auto object-contain !rounded-xl"
                   src={slidesImg}
                 />
               </SwiperSlide>
               <SwiperSlide className=" rounded-xl">
                 <Image
                   alt="img"
-                  className="w-full h-auto object-contain rounded-lg"
+                  className="w-full h-auto object-contain !rounded-xl"
                   src={slidesImg}
                 />
               </SwiperSlide>
               <SwiperSlide className=" rounded-xl">
                 <Image
                   alt="img"
-                  className="w-full h-auto object-contain rounded-lg"
+                  className="w-full h-auto object-contain !rounded-xl"
                   src={slidesImg}
                 />
               </SwiperSlide>
               <SwiperSlide className=" rounded-xl">
                 <Image
                   alt="img"
-                  className="w-full h-auto object-contain rounded-lg"
+                  className="w-full h-auto object-contain !rounded-xl"
                   src={slidesImg}
                 />
               </SwiperSlide>
               <SwiperSlide className=" rounded-xl">
                 <Image
                   alt="img"
-                  className="w-full h-auto object-contain rounded-lg"
+                  className="w-full h-auto object-contain !rounded-xl"
                   src={slidesImg}
                 />
               </SwiperSlide>
